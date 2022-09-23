fb7ee70f9bf7d1ae94687c83dcba4b2c0e622160
Ekaterina Nikitina <katya.adeeva@gmail.com>
2022-09-23 12:04:36 +0300

    двумерный массив

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Int number = Console.WriteLine(“Введите трехзначное число:”  ”);
+int a = number.ToString().Length;
+if (a <3 && a >3);
+{
+Console/WriteLine(“Вы ввели не трехзначное число:”  ”);
+}
+else
+{
+Console.WriteLine(“Введите number”);
+}
+
